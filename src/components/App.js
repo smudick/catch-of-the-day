89a1c5a6d6a745bca13bea8dd746d0cff083bdd1
Sam Mudick <smudick@gmail.com>
2020-11-09 15:56:39 -0600

    added local storage for order

--- src/components/App.js
+++ src/components/App.js
@@ -13,15 +13,26 @@ class App extends React.Component {
   };
   componentDidMount() {
     const { params } = this.props.match;
+    //first reinstate our localStorage
+    const localStorageRef = localStorage.getItem(params.storeId);
+    if(localStorageRef) {
+      this.setState({ order: JSON.parse(localStorageRef) });
+    }
     this.ref = base.syncState(`${params.storeId}/fishes`, {
       context: this,
       state: 'fishes'
     });
   };
 
+  componentDidUpdate() {
+    const { params } = this.props.match;
+    localStorage.setItem(this.props.match.params.storeId, JSON.stringify(this.state.order))
+  };
+
   componentWillUnmount() {
     base.removeBinding(this.ref);
   }
+
   
   addFish = (fish) => {
     // 1. take a copy of existing state
